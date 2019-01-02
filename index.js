6b1f9ae6c78a458df77c2cd5fa8125555c310fd8
Brian Marquis <rbmarquis.dev@gmail.com>
2019-01-01 23:57:40 -0600

    svg element with props and treemap group

--- index.js
+++ index.js
@@ -6,12 +6,43 @@ const VIDEO_GAME_SALES = "https://cdn.rawgit.com/freeCodeCamp/testable-projects-
 
 const dataUrls = [KICKSTARTER_PLEDGES, MOVIE_SALES, VIDEO_GAME_SALES];
 
+let data;
+
 /** Fetch json data, then... */
 Promise.all(dataUrls.map(url => d3.json(url)))
   .then((rcvdData) => {
      
     
-    console.log(rcvdData);
+    data = rcvdData;
   })
+  .then(() => drawSvg())
 ;
 
+/** Set properties for the svg element */
+const svgProps = {};
+svgProps.outerWidth = 1000;
+svgProps.outerHeight = svgProps.outerWidth / 1.6; // 16:10 aspect ratio
+svgProps.margin = {
+  top: svgProps.outerHeight * 0.05, 
+  right: svgProps.outerWidth * 0.05, 
+  bottom: svgProps.outerHeight * 0.05, 
+  left: svgProps.outerWidth * 0.05
+};
+svgProps.innerWidth = svgProps.outerWidth - svgProps.margin.left - svgProps.margin.right;
+svgProps.innerHeight = svgProps.outerHeight - svgProps.margin.top - svgProps.margin.bottom;
+
+function drawSvg() {
+  console.log(data);
+
+  const svg = d3.select("main div#svg-container")
+    .append("svg")
+    .attr("width", svgProps.outerWidth)
+    .attr("height", svgProps.outerHeight)
+  ;
+
+  const treemap = svg.append("g")
+    .attr("id", "treemap-diagram")
+    .attr("transform", "translate(" + svgProps.margin.left + ", " + svgProps.margin.top + ")")
+  ;
+
+}
